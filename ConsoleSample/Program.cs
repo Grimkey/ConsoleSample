2638bae55005f83c5517a28e33d02c71ddda0f15
James <Grimkey@gmail.com>
2015-05-21 14:41:07 -0700

    Adding yes / no question support.

--- ConsoleSample/Program.cs
+++ ConsoleSample/Program.cs
@@ -1,15 +1,34 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace ConsoleSample
+﻿namespace ConsoleSample
 {
-    class Program
+    using System;
+
+    public static class Program
     {
+        private static Random randomValue = new Random();
+
         static void Main(string[] args)
         {
+            var quit = false;
+
+            while (quit == false)
+            {
+                Console.Write("Ask a question: ");
+                var message = Console.ReadLine();
+                
+                if (string.Equals("quit", message, StringComparison.OrdinalIgnoreCase))
+                {
+                    quit = true;
+                }
+
+                Console.WriteLine(Response(message));
+            }
+        }
+
+        private static string Response(string inputmessage)
+        {
+            var answer = randomValue.Next(0, 2) == 0 ? "yes" : "no";
+
+            return string.Format("Hmm, '{0}'. I guess, {1}", inputmessage, answer);
         }
     }
 }
